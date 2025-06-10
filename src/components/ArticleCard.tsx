b2e49c9ab0a654909d22564bfe310e3686aac3c2
Revan Fazry Huda <rfazry10@gmail.com>
2025-06-10 11:32:28 +0700

    Update home

--- src/components/ArticleCard.tsx
+++ src/components/ArticleCard.tsx
@@ -1,11 +1,11 @@
 const ArticleCard = ({ title, date, content }: { title: string; date: string; content: string }) => {
-    return (
-      <div className="bg-white p-4 rounded-lg shadow-md mb-4">
-        <h3 className="text-lg font-semibold text-gray-800">{title}</h3>
-        <p className="text-gray-600 text-sm">{date}</p>
-        <p className="text-gray-700 mt-2">{content}</p>
-      </div>
-    );
-  };
-  
-  export default ArticleCard;
+  return (
+    <div className="bg-white p-4 rounded-lg shadow-md mb-4">
+      <h3 className="text-lg font-semibold text-gray-800">{title}</h3>
+      <p className="text-gray-600 text-sm">{date}</p>
+      <p className="text-gray-700 mt-2">{content}</p>
+    </div>
+  );
+};
+
+export default ArticleCard;
